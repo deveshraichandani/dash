c909bdb877c2450077e6ea797eeb4fe431b97e5a
Devesh <raichandanidevesh@gmail.com>
2023-07-25 23:00:15 +0530

    file commit 1

---
+++ bar_exercise.py
@@ -0,0 +1,15 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv("data/mocksurvey.csv",index_col=0)
+
+data = [go.Bar(x = df[response],
+               y = df.index,
+               name = response,
+               orientation="h") 
+               for response in df.columns]
+
+layout = go.Layout(title="Mock Survery", title_x = 0.5, barmode="stack")
+fig = go.Figure(data = data, layout = layout,)
+pyo.plot(fig, filename="mock survey.html")

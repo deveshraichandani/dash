2258662b133162ad886d30b4236adfb73c570f34
Devesh <raichandanidevesh@gmail.com>
2024-07-01 23:10:55 +0530

    second

--- box_plot_exercise.py
+++ box_plot_exercise.py
@@ -9,7 +9,7 @@ y1 = np.random.choice(df["rings"],200,replace="False")
 y2 = np.random.choice(df["rings"],150,replace="False")
 
 data = [go.Box(y = y1, name = "Sample 1"),
-        go.Box(y = y2, name = "Sample2")]
+        go.Box(y = y2, name = "Sample 2")]
 
 layout = go.Layout(title = "Comparison of two samples", title_x = 0.5,
                    yaxis = dict(title = "Ring Size"))
